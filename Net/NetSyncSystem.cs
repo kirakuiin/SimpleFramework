63a16d6df48d13da2ae06b9b42206115aaba5f6e
wang.zhuowei <549676201@qq.com>
2025-11-21 15:38:07 +0800

    feat(net): 添加网络同步系统基础框架
    - 实现NetSyncSystem类，提供网络数据同步功能
    - 监听连接事件并支持服务器创建、客户端连接、peer连接等事件
    - 集成协议处理器支持独立的数据包guard
    - 为网络数据同步奠定基础架构

--- Net/NetSyncSystem.cs
+++ Net/NetSyncSystem.cs
@@ -1,6 +1,58 @@
-﻿namespace SimpleFramework.Net;
+﻿using SimpleFramework.Net.Connection;
+using SimpleFramework.Utility;
 
-public class NetSyncSystem
+namespace SimpleFramework.Net;
+
+/// <summary>
+/// 一个用于同步网络间各种数据，状态的系统
+/// <para>底层通过监听网络连接事件，比如<see cref="PeerConnectedEvent"/>等事件来实现功能</para>
+/// <remarks>底层依赖<see cref="ConnectionModel"/></remarks>
+/// </summary>
+public class NetSyncSystem : AbstractSystem
 {
+    private ITransfer Transfer => this.GetUtility<ITransfer>();
     
+    private readonly ProtocolHandler _protocolHandler = new (NetDefine.SyncGuard);
+    
+    private readonly DisposableGroup _events =  new (); // 存储各种事件的引用
+    
+    protected override void OnInitialize()
+    {
+        this.ThrowIfNull<ConnectionModel>();
+        RegisterEvents();
+    }
+
+    protected override void OnUninitialize()
+    {
+        _events.Dispose();
+    }
+
+    private void RegisterEvents()
+    {
+        _events.Add(this.RegisterEvent<ServerCreateEvent>(OnServerCreate));
+        _events.Add(this.RegisterEvent<ClientConnectEvent>(OnClientConnect));
+        _events.Add(this.RegisterEvent<PeerConnectedEvent>(OnPeerConnected));
+        _events.Add(this.RegisterEvent<PeerDisconnectedEvent>(OnPeerDisconnected));
+        _events.Add(this.RegisterEvent<ServerDisconnectedEvent>(OnServerDisconnected));
+    }
+
+    private void OnServerCreate(ServerCreateEvent e)
+    {
+    }
+    
+    private void OnClientConnect(ClientConnectEvent e)
+    {
+    }
+    
+    private void OnPeerConnected(PeerConnectedEvent e)
+    {
+    }
+    
+    private void OnPeerDisconnected(PeerDisconnectedEvent e)
+    {
+    }
+    
+    private void OnServerDisconnected(ServerDisconnectedEvent e)
+    {
+    }
 }
