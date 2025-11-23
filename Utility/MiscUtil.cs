9cf71f6f70f22273f65d162965c078649e90671d
nico <549676201@qq.com>
2025-11-23 18:28:25 +0800

    feat(utility): 添加类型哈希功能到MiscUtil
    - 实现基于类型完整名称的哈希生成
    - 添加FNV-1a哈希算法支持
    - 提供泛型和非泛型的TypeHash方法
    - 增加类型唯一名称获取功能
    - 支持GUID生成和通用数据哈希计算
    
    为ProtocolHandler提供基础哈希支持，增强系统类型识别能力

--- Utility/MiscUtil.cs
+++ Utility/MiscUtil.cs
@@ -1,6 +1,78 @@
-﻿namespace SimpleFramework.Utility;
+﻿using System.Text;
 
-public class MiscUtil
+namespace SimpleFramework.Utility;
+
+/// <summary>
+/// 存放一些不太好分类的杂项功能
+/// </summary>
+public static class MiscUtil
 {
-    
+    /// <summary>
+    /// 获得类型的唯一名称
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static string GetUniqueTypeName<T>()
+    {
+        return GetUniqueTypeName(typeof(T));
+    }
+
+    /// <summary>
+    /// 获得类型的唯一名称
+    /// </summary>
+    /// <param name="type">类型</param>
+    /// <returns></returns>
+    public static string GetUniqueTypeName(Type type)
+    {
+        return $"{type.Assembly.GetName().Name}.{type.FullName}";
+    }
+
+    /// <summary>
+    /// 生成一个GUID
+    /// </summary>
+    /// <returns></returns>
+    public static Guid GenerateGuid()
+    {
+        return Guid.NewGuid();
+    }
+
+    /// <summary>
+    /// 获得类型的hash值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static ulong TypeHash<T>()
+    {
+        return TypeHash(typeof(T));
+    }
+
+    /// <summary>
+    /// 获得指定类型的hash值
+    /// </summary>
+    /// <param name="type">类型</param>
+    /// <returns></returns>
+    public static ulong TypeHash(Type type)
+    {
+        var typeName = GetUniqueTypeName(type);
+        return ComputeHash(typeName);
+    }
+
+    /// <summary>
+    /// 生成一个64位hash值
+    /// </summary>
+    /// <returns></returns>
+    public static ulong ComputeHash<T>(T data)
+    {
+        var bytes = SerializeUtil.Serialize(data);
+        const ulong offset = 14695981039346656037;
+        const ulong prime = 1099511628211;
+
+        var hash = offset;
+        foreach (byte b in bytes)
+        {
+            hash ^= b;
+            hash *= prime;
+        }
+        return hash;
+    }
 }
